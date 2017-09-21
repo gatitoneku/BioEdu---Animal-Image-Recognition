50835443b56256ef3dc7068b609fd8b6ad7a1812
gatitoneku <koboli@outlook.com>
2017-09-21 11:49:10 +0700

    new test

--- words.js
+++ words.js
@@ -1,10 +1,11 @@
 const katakerja = ['mengacak', 'mengaduk', 'menghisap', 'menyiram', 'menulis', 'melihat', 'menonton', 'memanggil', 'menangkis', 'melanggar', 'menyakiti', 'menghibur',
  'menyambungkan', 'mengajar', 'menlindungi', 'membakar', 'memandang', 'menyimpan', 'mengangkat', 'mencium', 'mengaduk', 'mendahului', 'menabrak', 'menanggulangi',
 'memperbaiki', 'merusak', 'mengurangi', 'menambah', 'menanam', 'memotong', 'meracik', 'merangkai', 'merampok', 'melamar', 'mengukur', 'menjaga', 'menggoreng', 'merebus',
-'membungkus', 'menggeser', 'mengguncang', 'membudidayakan', 'mengkebiri'];
+'membungkus', 'menggeser', 'mengguncang', 'membudidayakan', 'mengkebiri', 'mendorong', 'menarik', 'merevisi', 'mengkukus', 'menjilat', 'menyembah'];
 
 const katabenda = ['ayah', 'ayam', 'abang', 'bakso', 'ampas', 'arwah', 'asu', 'amerika', 'akar', 'abu', 'baju', 'beruang', 'ulat', 'babi ngepet', 'bantal', 'bandeng', 'babi', 'anjing', 'bintang', 'rusa',
 'kontol', 'kuntul', 'bebek', 'bangau', 'hak asasi manusia', 'bingkisan', 'botol', 'bajaj', 'konstitusi dan undang undang dasar', 'dendeng', 'kuda', 'mobil', 'motor', 'bis kuning',
-'makara', 'komputer', 'handphone', 'telepon', 'tv', 'radio', 'meja', 'kursi', 'lubang', 'kendaraan', 'obat', 'depok', 'jakarta', 'gajah'];
+'makara', 'komputer', 'handphone', 'telepon', 'tv', 'radio', 'meja', 'kursi', 'lubang', 'kendaraan', 'obat', 'depok', 'jakarta', 'gajah', 'kencing', 'kotoran', 'baju','furnitur', 'berak', 'tahi', 'kecoa',
+'kadal', 'macan', 'bangku taman', 'rumput', 'pohon', 'dedaunan', 'ikan', 'wajah anda', 'kebebasan berbicara', 'kue', 'RNG', 'kerupuk'];
 
 module.exports = { katakerja, katabenda }
