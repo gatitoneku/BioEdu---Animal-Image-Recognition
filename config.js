147b5a43fc39c514efef15d5fae31ab341d0d1e6
gatitoneku <koboli@outlook.com>
2017-12-05 16:26:37 +0700

    Fix mobile router

---
+++ config.js
@@ -0,0 +1,6 @@
+ const config = {
+    channelAccessToken: 'ty6j1NgXbxTY3sVWTk13TbOAvlC6TNtY0EBvSZw02JC+rCRLNcTioRERLgPrfvylOnwCk+rQJW7lYtkSidiH86mHUzkP1ERKcjcC2uLVNM4YfPpOmmeyhW6ojEpN5O/w0G8j/IoF08wRJIplxUfkDQdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '5140e8e814e0a4af5f0e8fc9e0e88753'
+  };
+
+  module.exports = {config};
