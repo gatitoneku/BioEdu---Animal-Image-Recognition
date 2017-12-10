32566fa01024843a5a93843682e799c64f81bc43
gatitoneku <koboli@outlook.com>
2017-12-10 20:29:43 +0700

    Fix mobile router

--- index.js
+++ index.js
@@ -73,7 +73,7 @@
     
              var replyArrayString = replyString.split(",")
     
-            //const echo = { type: 'text', text: "sepertinya itu adalah " + replyArrayString[0]};
+            const echo = { type: 'text', text: "sepertinya itu adalah " + replyArrayString[0]};
             const echo2 = { type: 'text', text: 'test'};
             console.log(echo);
             //res.send(JSON.stringify(replyArrayString[0]));
